1216ac76a20f78982d733a9124c5d379d30eaa84
simgeay <ay.simge@gmail.com>
2020-04-10 22:08:21 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 11;
+        String mesaj = "Ogrenci sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+}
