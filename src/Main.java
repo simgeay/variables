3ac500320c56acebcf9c8c57127c68352fd59d99
simgeay <ay.simge@gmail.com>
2020-04-10 22:19:22 +0200

    added comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        //variables
         int ogrenciSayisi = 11;
         String mesaj = "Ogrenci sayisi : ";
         System.out.println(mesaj + ogrenciSayisi);
